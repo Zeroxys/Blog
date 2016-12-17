7e4bb996c597a6e50e8a576428e788bc1b3973e4
Miguel Zavala <miguelzavalac@gmail.com>
2016-12-16 23:08:29 -0600

    Creacion server de dev con browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,32 @@
 const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const browserSync = require('browser-sync').create()
+const autoprefixer = require('autoprefixer')
+
+//servidor de desarrollo
+gulp.task('serve', function (){
+  browserSync.init({
+      server:{
+        baseDir:'./dist'
+      }
+    })
+})
+
+//tarea procesar css
+gulp.task('css', function(){
+  
+  var processors = [autoprefixer]
+
+  gulp.src('./src/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+})
+
+//tarea para vigilar cambios
+gulp.task('watch', function(){
+  gulp.watch('./src/*.css', ['css'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['watch','serve'])
